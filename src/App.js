b90b50ccc9f8116fffb19766b04e5028cbe9faaf
Marcos <marcossaiddiazc@gmail.com>
2021-12-28 00:28:48 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Coderhouse</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clase 2
         </p>
         <a
           className="App-link"
